e8fc9353575bab2dc09a540631595c05548a7177
cares0 <cares00000@gmail.com>
2022-07-25 20:39:55 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/com/triple/itinerary/ItineraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.itinerary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
